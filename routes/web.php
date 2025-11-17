00325ecba6f9dec17a7e5b655ec9c201fdf73ff7
Daniel Cubides <daniandrescubidesh@gmail.com>
2025-11-17 15:19:10 -0500

    Creación de rutas para los controladores de espacio y reserva

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\http\Controllers\espacioController;
+use App\http\Controllers\reservaController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('espacios', espacioController::class);
+Route::resource('reservas', reservaController::class);
